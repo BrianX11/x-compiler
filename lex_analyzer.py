67a83e2da20eff36f0010abdf885bcab6d31b3d1
BrianX11 <brianx0311@gmail.com>
2023-02-26 12:53:37 -0400

    Implement Editor

--- lex_analyzer.py
+++ lex_analyzer.py
@@ -5,38 +5,37 @@ from token_type.identification import isID
 from token_type.number import isNumber
 from token_type.string import isString, isStringStart
 
-source_code = open("example.x", "r")
-line = "None"
-
-tokens = ("", "")
-pattern = ""
-lexeme = ""
-
-for line in source_code:
-    temp = ""
-    for pos, current in enumerate(line):
-        if isDelimiter(current) or isDelimiter(temp):
-            if temp:
-                if isOperator(temp+current) or isStringStart(temp):
-                    temp += current
+def parse(input):
+    tokens=[]
+    line_n = 0
+    for line_n, line in enumerate(input):
+        temp = ""
+        start = 0
+        for pos, current in enumerate(line + ' '):
+            if isDelimiter(current) or isDelimiter(temp):
+                if temp:
+                    if isOperator(temp+current) or isStringStart(temp):
+                        temp += current
+                    else:
+                        token_type = (
+                            ("KEYWORD", isKeyword(temp), "cyan"),
+                            ("ID", isID(temp), "yellow"),
+                            ("NUMBER", isNumber(temp), "green"),
+                            ("STRING", isString(temp), "green"),
+                            ("OPERATOR", isOperator(temp), "red"),
+                            ("DELIMITER", isDelimiter(temp), "magenta"),
+                            ("UNKNOWN", None)
+                        )
+                        token = next((t for t in token_type if t[1]), token_type[-1])
+                        if temp != delimiters["WHITESPACE"]:
+                            tokens.append((temp, token[0], token[1], start, pos, line_n, token[2]))
+                            print((temp, token[0], token[1], start, pos, line_n))
+                        start = pos
+                        temp = current
                 else:
-                    token_type = (
-                        ("KEYWORD", isKeyword(temp)),
-                        ("ID", isID(temp)),
-                        ("NUMBER", isNumber(temp)),
-                        ("STRING", isString(temp)),
-                        ("OPERATOR", isOperator(temp)),
-                        ("DELIMITER", isDelimiter(temp)),
-                        ("UNKNOWN", None)
-                    )
-                    token = next((t for t in token_type if t[1]), token_type[-1])
-                    if temp != delimiters["WHITESPACE"]:
-                        print((temp, token[0], token[1]))
-                    temp = current
+                    temp += current
             else:
                 temp += current
-        else:
-            temp += current
-        
+    return tokens
 
             
---
+++ main.py
@@ -0,0 +1,101 @@
+import tkinter as tk
+from tkinter import ttk, filedialog
+import lex_analyzer as lex
+import uuid
+
+class Tooltip:
+    active_tip = None
+
+    def __init__(self, widget, text):
+        self.widget = widget
+        self.text = text
+        self.tip = None
+        self.id = None
+        self.x = self.y = 0
+        
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+
+    def enter(self, event=None):
+        if Tooltip.active_tip:
+            Tooltip.active_tip.hide_tip()
+        self.show_tip(event.x_root + 10, event.y_root + 10)
+
+    def leave(self, event=None):
+        self.hide_tip()
+
+    def show_tip(self, x, y):
+        if self.tip:
+            return
+        self.x, self.y = x, y
+        self.tip = tk.Toplevel(self.widget)
+        self.tip.wm_overrideredirect(True)
+        self.tip.wm_geometry("+%d+%d" % (x, y))
+        label = tk.Label(self.tip, text=self.text, justify=tk.LEFT,
+                         background="#ffffe0", relief=tk.SOLID, borderwidth=1,
+                         font=("tahoma", "8", "normal"))
+        label.pack(ipadx=1)
+        Tooltip.active_tip = self
+
+    def hide_tip(self):
+        if self.tip:
+            self.tip.destroy()
+        self.tip = None
+
+class Editor:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Compilador X")
+        #self.master.state('zoomed')
+        self.screen_width = self.master.winfo_screenwidth()
+        self.screen_height = self.master.winfo_screenheight()
+        self.minimized_width = int(self.screen_width // 1.4)
+        self.minimized_height = int(self.screen_height // 1.4)
+        self.master.minsize(self.minimized_width, self.minimized_height)
+        
+        self.text = tk.Text(self.master, undo=True, maxundo=-1, autoseparators=True)
+        self.text.pack(expand=True, fill="both")
+        self.scrollbar = tk.Scrollbar(self.text)
+        self.scrollbar.pack(side="right", fill="y")
+        self.text.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.text.yview)
+
+        self.toolbar = ttk.Frame(self.master)
+        self.toolbar.pack(side="top", fill="x")
+
+        self.open_button = ttk.Button(self.toolbar, text="Open File", command=self.open_file)
+        self.open_button.pack(side="left")
+
+        self.parse_button = ttk.Button(self.toolbar, text="Analizador Lexico", command=self.on_button_click)
+        self.parse_button.pack(side="left")
+    
+    def on_button_click(self):
+        tokens = lex.parse(self.text.get("1.0", "end").splitlines())
+        for token in tokens:
+            self.highlight(token[3], token[4], token[5], token[6])
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            with open(file_path, "r") as file:
+                file_contents = file.read()
+                self.text.delete("1.0", "end")
+                self.text.insert("end", file_contents)
+    
+    def highlight(self, start_position, end_position, line_number, color):
+        start_index = f"{line_number+1}.{start_position}"
+        end_index = f"{line_number+1}.{end_position}"
+        uu = uuid.uuid4()
+        tip = Tooltip(self.text, "This is a tooltip")
+        self.text.tag_add(uu, start_index, end_index)
+        self.text.tag_configure(uu, background=color)
+        self.text.tag_bind(uu, "<Enter>", tip.enter)
+        self.text.tag_bind(uu, "<Leave>", tip.leave)
+
+def main():
+    root = tk.Tk()
+    Editor(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
